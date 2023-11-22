880f7bda155aff72d08099be2b139935afe90dd8
greg <gthrone25@gmail.com>
2023-11-22 11:41:20 -0600

    section 58

---
+++ section58/public/javascripts/clusterMap.js
@@ -0,0 +1,18 @@
+var map = L.map('map').setView([35.6876096, -105.938456], 4);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var markers = new L.MarkerClusterGroup();
+
+for (let campground of campgrounds) {
+    let title = `${campground.properties.popUpMarkup}`
+    // console.log(title)
+    let marker = L.marker(campground.geometry.coordinates);
+    // markers.addLayer(L.marker([175.3107, -37.7784]));
+    marker.bindPopup(title);
+    markers.addLayer(marker);
+}
+
+map.addLayer(markers);
