b0c04843bb8985b93657eee621625a659dc0b1e3
greg <gthrone25@gmail.com>
2023-11-27 11:17:36 -0600

    section 67

---
+++ section67/state-demo/src/Dumbo.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+function generateGameBoard() {
+    console.log("making a gameboard");
+    return Array(5000);
+}
+
+export default function Dumbo() {
+    const [board, setBoard] = useState(generateGameBoard);
+    return (
+        <button onClick={() => setBoard("hello")}>Click me to change state</button>
+    )
+}
